1069117d9afa589cbe354f3a38af49067a42f5e6
subham007-coder <dsubham109@gmail.com>
2025-09-27 00:34:47 +0530

    back to safe point

--- models/Product.js
+++ models/Product.js
@@ -1,29 +1,5 @@
 const mongoose = require("mongoose");
 
-const variantSchema = new mongoose.Schema({
-  length: {
-    type: String,
-    required: true,
-  },
-  curl: {
-    type: String,
-    required: true,
-  },
-  stock: {
-    type: Number,
-    required: true,
-    default: 0,
-  },
-  sku: {
-    type: String,
-    required: false,
-  },
-  barcode: {
-    type: String,
-    required: false,
-  },
-});
-
 const productSchema = new mongoose.Schema(
   {
     productId: {
@@ -91,7 +67,7 @@ const productSchema = new mongoose.Schema(
         required: false,
       },
     },
-    variants: [variantSchema],
+    variants: [{}],
     isCombination: {
       type: Boolean,
       required: true,

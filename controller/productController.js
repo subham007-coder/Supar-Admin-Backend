9f5d7b4f8b53200bb060d040f51db21fcf78ade4
subham007-coder <dsubham109@gmail.com>
2025-10-21 22:02:10 +0530

    add logos to undurstand the stock problem

--- controller/productController.js
+++ controller/productController.js
@@ -6,6 +6,10 @@ const { languageCodes } = require("../utils/data");
 
 const addProduct = async (req, res) => {
   try {
+    console.log('DEBUG - addProduct - Request body:', JSON.stringify(req.body));
+    console.log('DEBUG - addProduct - Request body variants:', JSON.stringify(req.body.variants));
+    console.log('DEBUG - addProduct - Request body isCombination:', req.body.isCombination);
+    
     // Get the English title or the first available title
     const title = req.body.title.en || Object.values(req.body.title)[0];
     
@@ -27,7 +31,7 @@ const addProduct = async (req, res) => {
       isCombination: req.body.variants && req.body.variants.length > 0 ? true : req.body.isCombination,
     };
 
-    console.log('Creating product with data:', {
+    console.log('DEBUG - addProduct - Creating product with data:', {
       title: productData.title,
       isCombination: productData.isCombination,
       variants: productData.variants,
@@ -35,11 +39,17 @@ const addProduct = async (req, res) => {
     
     // Log the full variants data for debugging
     if (productData.variants && productData.variants.length > 0) {
-      console.log('Variants data:', JSON.stringify(productData.variants));
+      console.log('DEBUG - addProduct - Variants data:', JSON.stringify(productData.variants));
+    } else {
+      console.log('DEBUG - addProduct - No variants found in request or variants array is empty');
+      console.log('DEBUG - addProduct - Raw variants from request:', req.body.variants);
+      console.log('DEBUG - addProduct - Type of variants:', typeof req.body.variants);
     }
 
     const newProduct = new Product(productData);
+    console.log('DEBUG - addProduct - Product model before save:', JSON.stringify(newProduct));
     const product = await newProduct.save();
+    console.log('DEBUG - addProduct - Saved product:', JSON.stringify(product));
     res.status(201).send(product);
     
   } catch (err) {
